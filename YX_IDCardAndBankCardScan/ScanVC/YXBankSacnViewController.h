7a8122558d8097611ac93fb47a442f021317c8e0
admin <admin@admindeiMac.local>
2017-10-27 17:56:33 +0800

    first commit

---
+++ YX_IDCardAndBankCardScan/ScanVC/YXBankSacnViewController.h
@@ -0,0 +1,13 @@
+//
+//  YXBankSacnViewController.h
+//  YX_IDCardAndBankCardScan
+//
+//  Created by admin on 2017/10/27.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YXBankSacnViewController : UIViewController
+
+@end
---
+++ YX_IDCardAndBankCardScan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YX_IDCardAndBankCardScan
+//
+//  Created by admin on 2017/10/27.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
